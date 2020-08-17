37afc5e14222d2fc11208bb9f82332d5f987a5ca
Arun <arun.ranga@hotmail.ca>
2020-08-16 22:25:31 -0400

    Updates

---
+++ precache-manifest.1ff12c6dbd37f53983b7780dea9f602a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97473898599284c566b82bed2ea6524e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f634e4a320f9e0871590",
+    "url": "/static/css/2.af0132db.chunk.css"
+  },
+  {
+    "revision": "a22508d09bc2c3fc8ce2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f634e4a320f9e0871590",
+    "url": "/static/js/2.9c9704c9.chunk.js"
+  },
+  {
+    "revision": "908678959397f2e04ac5f8f7afda467f",
+    "url": "/static/js/2.9c9704c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a22508d09bc2c3fc8ce2",
+    "url": "/static/js/main.d67be2e4.chunk.js"
+  },
+  {
+    "revision": "506adfa26ad76816c4a2",
+    "url": "/static/js/runtime-main.c34c0ec6.js"
+  },
+  {
+    "revision": "48ebd5f59989c4ef1497350c9377d396",
+    "url": "/static/media/resume.48ebd5f5.pdf"
+  }
+]);
