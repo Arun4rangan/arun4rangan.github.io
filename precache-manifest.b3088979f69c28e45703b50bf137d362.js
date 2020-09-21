d04f4c761689e75eb9b2da7f0a7f14deb3cc099a
Arun Ranganathan <arun.ranga@hotmail.ca>
2020-09-20 23:26:47 -0400

    Updates

---
+++ precache-manifest.b3088979f69c28e45703b50bf137d362.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24370926bf3d62dd04e595614ab7d56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e287fbf481fd6816bf6",
+    "url": "/static/css/2.af0132db.chunk.css"
+  },
+  {
+    "revision": "6bdae2bc0546646d3671",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1e287fbf481fd6816bf6",
+    "url": "/static/js/2.e5c2b043.chunk.js"
+  },
+  {
+    "revision": "908678959397f2e04ac5f8f7afda467f",
+    "url": "/static/js/2.e5c2b043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bdae2bc0546646d3671",
+    "url": "/static/js/main.d3d5506e.chunk.js"
+  },
+  {
+    "revision": "506adfa26ad76816c4a2",
+    "url": "/static/js/runtime-main.c34c0ec6.js"
+  },
+  {
+    "revision": "1635d112a9251023bbe73ba9c5a0f4a5",
+    "url": "/static/media/resume.1635d112.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9bdb2b575ac902a5a4e1f586263aeb1e.js"
+  "/precache-manifest.b3088979f69c28e45703b50bf137d362.js"
 );
 
 self.addEventListener('message', (event) => {
